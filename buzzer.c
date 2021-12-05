ba87a01067a4e0e8ab495f9a775967f96222beba
jiseok60171861 <wltjr7859@naver.com>
2021-12-05 22:26:40 +0900

    asdasfas
    file:///home/wtwt/emb2021/buzzer.c
    file:///home/wtwt/emb2021/buzzer.h
    file:///home/wtwt/emb2021/buzzertest.c

--- buzzer.c
+++ buzzer.c
@@ -6,15 +6,18 @@
 #include <dirent.h>
 #include "buzzer.h"
 
-#define MAX_SCALE_STEP 8
+#define MAX_SCALE_STEP 13
 #define BUZZER_BASE_SYS_PATH "/sys/bus/platform/devices/"
 #define BUZZER_FILENAME "peribuzzer"
 #define BUZZER_ENABLE_NAME "enable"
 #define BUZZER_FREQUENCY_NAME "frequency"
+
 static const int musicScale[MAX_SCALE_STEP] =
 {
-    262, /*do*/ 294,330,349,392,440,494, /* si */ 523
+    262, /*do*/ 294,330,349,392,440,494, /* si */ 523, 329, 350, 245, 213, 262
 };
+
+
 
 static char gBuzzerBaseSysDir[128]; ///sys/bus/platform/devices/peribuzzer.XX 가 결정됨
 static int fd_en, fd_freq;
@@ -78,4 +81,98 @@ int buzzerExit(void)           // buzzer exit
     close(fd_freq);
     close(fd_en);
     return 0;
+}
+int BGM1(void)
+{
+    buzzerPlaySong(musicScale[4]);
+    usleep(300000);
+    buzzerPlaySong(musicScale[8]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[9]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[4]);
+    usleep(300000);
+    buzzerPlaySong(musicScale[8]); 
+    usleep(150000);
+    buzzerPlaySong(musicScale[9]); 
+    usleep(150000);
+    buzzerPlaySong(musicScale[4]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[10]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[11]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[10]); 
+    usleep(150000);
+    buzzerPlaySong(musicScale[12]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[2]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[3]); 
+    usleep(150000);
+    buzzerPlaySong(musicScale[2]); 
+    usleep(300000);
+    buzzerPlaySong(musicScale[12]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[2]);
+    usleep(150000);
+    buzzerStopSong(); // 부저 끄기
+}
+int BGM2(void)
+{
+    int i = 0;
+    for(i = 0; i < 12; i++)
+    {
+        buzzerPlaySong(musicScale[4]); // 솔 12번
+        usleep(200000);
+        buzzerStopSong();
+        usleep(50000);
+    }
+    for(i = 0; i < 6; i++)
+    {
+        buzzerPlaySong(musicScale[6]); // 시 6번
+        usleep(200000);
+        buzzerStopSong();
+        usleep(50000);
+    }
+    for(i = 0; i < 3; i++)
+    {
+        buzzerPlaySong(musicScale[7]); // 도 도 도
+        usleep(200000);
+        buzzerStopSong();
+        usleep(50000);
+    }
+    buzzerPlaySong(musicScale[7]); // 도
+    usleep(300000);
+    buzzerPlaySong(musicScale[6]); // 시
+    usleep(300000);
+    buzzerPlaySong(musicScale[5]); // 라
+    usleep(300000);
+    for(i = 0; i < 12; i++)
+    {
+        buzzerPlaySong(musicScale[4]); // 솔 12번
+        usleep(200000);
+        buzzerStopSong();
+        usleep(50000);
+    }
+    for(i = 0; i < 6; i++)
+    {
+        buzzerPlaySong(musicScale[6]); // 시 6번
+        usleep(200000);
+        buzzerStopSong();
+        usleep(50000);
+    }
+    buzzerPlaySong(musicScale[7]); // 도
+    usleep(300000);
+    buzzerPlaySong(musicScale[4]); // 솔
+    usleep(300000);
+    buzzerPlaySong(musicScale[2]); // 미
+    usleep(300000);
+    buzzerPlaySong(musicScale[0]); // 도
+    usleep(600000);
+    buzzerStopSong(); // 부저 끄기
 }
--- buzzer.h
+++ buzzer.h
@@ -5,5 +5,7 @@ int buzzerInit(void);           // buzzer 포함 함수
 int buzzerPlaySong(int scale);  // buzzer on
 int buzzerStopSong(void);       // buzzer off
 int buzzerExit(void);           // buzzer exit
+int BGM1(void); // 프로잭트 bgm 캐논변주곡
+int BGM2(void); // 프로잭트 bgm 젓가락행진곡
 
 #endif
